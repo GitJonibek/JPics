5877346eb6132ec649d6196f3dd8b4b57b3077f6
GitJonibek <jonibeknorboev@gmail.com>
2018-04-28 02:24:37 +0500

    First commit with an example - JPics custom library to load the image from URL into ImageView fast and easy way.

---
+++ jpics/JPics.java
@@ -0,0 +1,63 @@
+package com.example.jonib.jpics.jpics;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+public class JPics {
+
+    private Context context;
+    private String url;
+    private DownloadManager.OnCompleteListener<Bitmap> onCompleteListener;
+
+    private JPics(Context context) {
+        this.context = context;
+    }
+
+    public static JPicsHider with(Context context) {
+        return new JPicsHider(context);
+    }
+
+    public JPicsDownloadTask into(final ImageView imageView) {
+        return DownloadManager.getInstance().downloadImage(url, imageView, onCompleteListener);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public AsyncTask<String, Void, String> downloadPDFile(){
+        return new DownloadManager.DownloadFileAsyncTask().execute(this.url);
+    }
+
+    private JPics setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public static class JPicsHider {
+        private Context context;
+
+        private JPicsHider(Context context) {
+            this.context = context;
+        }
+
+        public JPics setUrl(String url) {
+            JPics jPics = new JPics(context);
+            return jPics.setUrl(url);
+        }
+
+    }
+
+    public static class Config{
+
+        public static void setCacheCapacity(int capacity) {
+            DownloadManager.getInstance().setCacheCapacity(capacity);
+        }
+        public static int getCacheCapacity() {
+            return DownloadManager.getInstance().getCacheCapacity();
+        }
+    }
+
+}
